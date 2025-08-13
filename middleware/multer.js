e35a80bdde3ac9e5c1dae58c732f03130eb79597
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-08-13 18:26:10 +0300

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,42 +1,21 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
 
-const Istorage = multer.diskStorage({
+ cloudinary.config({ 
+        cloud_name: 'drie0h3iv', 
+        api_key: '987485225419974', 
+        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+    });
 
-     destination: (req, file, cb) => {
-      cb(null, path.join(__dirname, '../uploads'));
-    },
-    
-    
-    filename:(req,file,cb)=>{
-        var extention = path.extname(file.originalname)
-        cb(null, Date.now() + extention)
-    }
-})
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads', // اسم الفولدر في Cloudinary
+    resource_type: 'auto' // يسمح برفع الصور والفيديوهات والـ pdf
+  }
+});
 
-const Iupload=multer({
-    storage:Istorage,
-    limits:{fileSize: 1024 * 1024 * 1024},
-    fileFilter:(req,file,cb)=>{
-	console.log(`File type: ${file.mimetype}`);
-        fileType = file.mimetype == "image/png" || file.mimetype ==  "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "video/mp4"||file.mimetype == 'video/gif'||file.mimetype === 'video/webm'||file.mimetype === 'video/ogg'||file.mimetype === "application/pdf"
-            
-        if(fileType){
-	    console.log('Valid file type');
-            cb(null,true)
-        }
-        else{
-	    console.log('Invalid file type');
-            cb(null,false)
-        }
-        }
+const upload = multer({ storage: storage });
 
-
-})
-
-
-module.exports = Iupload ; 
-
-
-
-
+module.exports = upload;
