14931838450e658899d9e54eded4a7e742686646
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-08-13 18:47:34 +0300

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,7 +5,7 @@ const cloudinary = require('cloudinary').v2;
  cloudinary.config({ 
         cloud_name: 'drie0h3iv', 
         api_key: '987485225419974', 
-        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+        api_secret: 'DLZl4mj7EBNkLO4e4H9vIbt5xcM' // Click 'View API Keys' above to copy your API secret
     });
 
 const storage = new CloudinaryStorage({
