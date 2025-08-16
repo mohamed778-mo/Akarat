31a63df6e7819f5ab5ae3cce9a5ad50588556544
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-08-16 22:07:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -24,26 +24,28 @@ app.use(helmet({
 
 
 
+const allowedOrigins = ['https://donlyhelio.egyptge.com', 'https://onlyhelio.egyptge.com', 'https://bonlyhelio.egyptge.com', ];
 
-const allowedOrigins = [
-    'https://akarat-front.vercel.app',
-    'https://akarat-six.vercel.app',
-    'https://helioin.vercel.app'
-];
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  }
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, 
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions)); 
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 
 
