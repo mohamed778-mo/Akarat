78455d985c07e99251fdd917e7dbc55f5f3ac161
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-08-16 21:56:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,21 +25,25 @@ app.use(helmet({
 
 
 
-const allowedOrigins = [ 'https://akarat-front.vercel.app','https://akarat-six.vercel.app','https://helioin.vercel.app' ];
+const allowedOrigins = [
+    'https://akarat-front.vercel.app',
+    'https://akarat-six.vercel.app',
+    'https://helioin.vercel.app'
+];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  }
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, 
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions)); 
 
 
 
