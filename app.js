66685ff7d64b1ef99fdf8304efc7af3afa4b5343
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-08-16 20:11:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(helmet({
 
 
 
-const allowedOrigins = [ 'https://akarat-front.vercel.app','https://akarat-six.vercel.app' ];
+const allowedOrigins = [ 'https://akarat-front.vercel.app','https://akarat-six.vercel.app','https://helioin.vercel.app' ];
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
